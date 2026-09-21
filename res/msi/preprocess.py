a1468a2a6870d87c5832f0ac8b332ce1eb0294ed
Cleilson <137639044+cleilsonalvino@users.noreply.github.com>
2026-09-21 08:53:08 -0300

    Update branding from Purslane Tech to Quero Delivery in project metadata and resources. Change company name, product name, and legal copyright in Cargo.toml, Runner.rc, and other relevant files to reflect the new branding for QueroDesk.

--- res/msi/preprocess.py
+++ res/msi/preprocess.py
@@ -93,7 +93,7 @@ def make_parser():
         "-m",
         "--manufacturer",
         type=str,
-        default="Purslane Tech Pte. Ltd.",
+        default="Quero Delivery",
         help="The app manufacturer.",
     )
     return parser
